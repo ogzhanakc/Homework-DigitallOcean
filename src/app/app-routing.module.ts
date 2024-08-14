1e00f5f971bc1ae7efdcb9088029828c886327c8
Ogzhanakc <ogzhan.akc@gmail.com>
2024-08-14 08:30:22 +0300

    App module and App routing module created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { Routes } from '@angular/router';
+
+
+
+export const routes: Routes = [];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
